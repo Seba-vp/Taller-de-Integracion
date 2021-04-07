80140707ad3491c764ea85cffd606a6f54a41432
Seba <svillablanca1@uc.cl>
2021-04-06 22:41:07 -0400

    agregue la carpeta de la app

---
+++ app_ti/home/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='page-home'),
+    path('season/<str:serie>/<int:number>', views.season, name='page-season'),
+    path('episode/<int:episode_id>', views.episode, name='page-episode'),
+    path('character/<str:char_name>', views.character, name='page-character'),
+]
---
+++ app_ti/home/views.py
@@ -0,0 +1,123 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import requests
+
+# Create your views here.
+
+
+seasons = [
+    {
+        'number':'1',
+        'year':'2008',
+        'serie':'Breaking Bad',
+            },
+    {
+        'number':'2',
+        'year':'2009',
+        'serie':'Breaking Bad',
+            },
+    {
+        'number':'3',
+        'year':'2010',
+        'serie':'Breaking Bad',
+            },
+    {
+        'number':'4',
+        'year':'2011',
+        'serie':'Breaking Bad',
+            },
+    {
+        'number':'5',
+        'year':'2012 - 2013',
+        'serie':'Breaking Bad',
+            },
+    {
+        'number':'1',
+        'year':'2015',
+        'serie':'Better Call Saul',
+            },
+    {
+        'number':'2',
+        'year':'2016',
+        'serie':'Better Call Saul',
+            },
+    {
+        'number':'3',
+        'year':'2017',  
+        'serie':'Better Call Saul',
+            },
+    {
+        'number':'4',
+        'year':'2018',        
+        'serie':'Better Call Saul',
+            },
+    {
+        'number':'5',
+        'year':'2020',
+        'serie':'Better Call Saul',
+            },
+]
+
+
+#response = requests.get('http://tarea-1-breaking-bad.herokuapp.com/api/episodes').json()
+
+def home(request):
+    
+    context = {
+        'seasons': seasons,
+        'title': 'Breaking App - Home'
+    }
+    return render(request, 'home.html', context)
+
+
+def season(request, serie, number):
+    episodes = requests.get('http://tarea-1-breaking-bad.herokuapp.com/api/episodes').json()
+
+    e_list = []
+
+    for e in episodes:
+        if int(e['season']) == number and e['series'] == serie:
+            e_list.append(e)
+
+    context = {
+        'serie': serie,
+        'number': number,
+        'e_list': e_list,
+        'title': 'Breaking App - Season'
+    }
+    
+    return render(request, 'season.html', context)
+
+def episode(request, episode_id):
+    episode = requests.get('http://tarea-1-breaking-bad.herokuapp.com/api/episodes/'+str(episode_id)).json()
+
+    e_info = episode[0]
+
+    print(e_info)
+
+    context = {
+        'e_info': e_info,
+         'title': 'Breaking App - Episode'
+    }
+    
+    return render(request, 'episode.html', context)
+
+
+def character(request, char_name):
+    li=char_name.split()
+
+    character_raw = requests.get('http://tarea-1-breaking-bad.herokuapp.com/api/characters?name='+'+'.join(li)).json()
+    quotes = requests.get('http://tarea-1-breaking-bad.herokuapp.com/api/quote?author='+'+'.join(li)).json()
+
+    character = character_raw[0]
+    
+    print(quotes)
+
+    context = {
+        'character': character,
+        'quotes': quotes,
+         'title': 'Breaking App - Character'
+    }
+    
+    return render(request, 'character.html', context)
